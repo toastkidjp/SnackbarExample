6ad405f4640011fcd3f430b8911a28b4dbeb321b
Toast Kid <super_q1m@yahoo.co.jp>
2017-06-13 19:44:48 +0900

    Initial commit.

--- app/src/main/java/jp/toastkid/snackbarexample/DummyActivity.java
+++ app/src/main/java/jp/toastkid/snackbarexample/DummyActivity.java
@@ -3,32 +3,83 @@ package jp.toastkid.snackbarexample;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
+ * Dummy activity for using Snackbar on App-Widget.
+ *
  * @author toastkidjp
  */
 public class DummyActivity extends AppCompatActivity {
 
+    /** Key of Extra. */
     public static final String KEY_COLOR = "color";
+
+    /** Hold for using Snackbar. */
     private View view;
+
+    /** Snackbar's color. */
+    private int mColor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        view = new View(this);
+        view = makeDummyView();
         setContentView(view);
+        overridePendingTransition(0, 0);
+
+        mColor = getIntent().getIntExtra(KEY_COLOR, Color.BLACK);
+        setStatusBarTransparent();
+        executeFinisher();
+    }
+
+    private View makeDummyView() {
+        final View view = new View(this);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        return view;
+    }
+
+    private void executeFinisher() {
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        });
+        executorService.shutdown();
+    }
+
+    private void setStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(mColor);
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        final int color = getIntent().getIntExtra(KEY_COLOR, Color.BLACK);
-        SnackbarFactory.make(view, color).show();
+        SnackbarFactory.make(view, mColor).show();
     }
 
     @Override
--- app/src/main/java/jp/toastkid/snackbarexample/SnackbarFactory.java
+++ app/src/main/java/jp/toastkid/snackbarexample/SnackbarFactory.java
@@ -2,20 +2,27 @@ package jp.toastkid.snackbarexample;
 
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author toastkidjp
  */
-public class SnackbarFactory {
+class SnackbarFactory {
 
-    public static Snackbar make(final View v, final int color) {
+    /**
+     * Make Snackbar object with specified color.
+     * @param v
+     * @param color
+     * @return
+     */
+    static Snackbar make(final View v, final int color) {
         final Snackbar snackbar
                 = Snackbar.make(v, "ここにテキストが入ります。", Snackbar.LENGTH_SHORT);
         snackbar.getView().setBackgroundColor(color);
-        snackbar.setAction("再表示", new View.OnClickListener() {
+        snackbar.setAction("Next!", new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                snackbar.show();
+                Toast.makeText(v.getContext(), "Why?", Toast.LENGTH_SHORT).show();
             }
         });
         return snackbar;
